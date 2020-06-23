25dd7913f6c0c8de342c34a70fe6fbe97f79fdfb
Shiva Tomar <67202813+ShivaTomar@users.noreply.github.com>
2020-06-23 23:40:07 +0530

    Add files via upload

---
+++ Data Structrues/Stack/Implementation of stack (Arrays).java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class stack{
+    int arr[];
+    int topOfStack;
+
+    public stack(int size){
+        this.arr=new int[size];
+        this.topOfStack=-1;
+    }
+
+    public void push(int data){
+
+        if(isFull()) System.out.println("Stack Overflow");
+
+        else{
+            arr[topOfStack+1]=data;
+            topOfStack++;
+        }
+    }
+
+    public void pop(){
+
+        if(isEmpty()) System.out.println("Stack Underflow");
+
+        else{
+            System.out.println(arr[topOfStack]);
+            arr=Arrays.copyOf(arr, arr.length-1);
+            topOfStack--;
+        }
+    }
+
+    public void delete(){
+        arr=null;
+    }
+
+    public void peek(){
+        if(!isEmpty()) System.out.println("peek of stack="+arr[topOfStack]);
+        else System.out.println("Stack is empty");
+    }
+
+    public boolean isEmpty(){
+        if(topOfStack==-1) return true;
+        return false;
+    }
+
+    public boolean isFull(){
+        if(topOfStack==arr.length) return true;
+        return false;
+    }
+}
+
+class runner{
+    public static void main(String[] args){
+
+        stack st=new stack(5);
+        st.push(5);
+        st.push(4);
+        st.push(10);
+        st.push(15);
+        st.push(20);
+        st.pop();
+        st.pop();
+        st.pop();
+        st.pop();
+        st.peek();
+
+
+    }
+}
